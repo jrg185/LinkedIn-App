247fcdeca31c73d6029455a5309dd2e80f69503c
jrg185 <117409560+jrg185@users.noreply.github.com>
2022-12-07 00:49:45 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -19,54 +19,107 @@ def probs(data):
     return prob.predict_proba(data)
 
 
+st.image(image = "https://upload.wikimedia.org/wikipedia/commons/0/01/LinkedIn_Logo.svg")
+
 st.markdown("# LinkedIn User Prediction App")
-st.markdown("## This app is to predict whether or not you are a LinkedIn User based on multiple variables")
+st.markdown("### The purpose of this app is to test a Machine Learning classification model, predicting whether or not you are a LinkedIn user based on 6 variables.  All responses are completely anonymous.")
 
 st.header("User Inputs")
-col3, col4 = st.columns(2)
-with col3:
-    st.markdown("***Income Reference (in USD)***")
-    st.markdown(" 1 < 10k")
-    st.markdown(" 2 10k - 20k")
-    st.markdown(" 3 20k - 30k")
-    st.markdown(" 4 30k - 40k")
-    st.markdown(" 5 40k - 50k")
-    st.markdown(" 6 50k - 75k")
-    st.markdown(" 7 75k - 100k")
-    st.markdown(" 8 100k - 150k")
-    st.markdown(" 9 150k or more")
-
-with col4: 
-    st.markdown("***Education Level Reference***")
-    st.markdown(" 1 Less than Highschool (Grades 1-8 or no formal schooling)")
-    st.markdown(" 2 High School Incomplete (Grades 9-11 or Grade 12 with NO diploma)")
-    st.markdown(" 3 High School Graduate (Grade 12 with diploma or GED certificate)")
-    st.markdown(" 4 Some college, no degree (includes some community college)")
-    st.markdown(" 5 Two-year associate degree from a college or university")
-    st.markdown(" 6 Four-year college or university degree/Bachelor’s degree (e.g., BS, BA, AB)")
-    st.markdown(" 7 Some postgraduate or professional schooling, no postgraduate degree (e.g. some graduate school)")
-    st.markdown(" 8 Postgraduate or professional degree, including master’s, doctorate, medical or law degree (e.g., MA, MS, PhD, MD, JD)")
-    
 
 
 col1, col2 = st.columns(2)
 with col1:
-    income = st.slider('Income Level(1-9)',1,9,1)
-    education = st.slider('Education Level(1-8)', 1,8,1)
-    parent = st.slider('Parent(1 = yes, 0 = no)',0,1,1)
+    income = st.selectbox('What Is Your Income Range?',['<$10k', '$10k-$20k', '$20k-$30k', '$30k-$40k', '$40k-$50k','$50k-$75k','$75k-$100k','$100k-$150k','$150k +'])
+    education = st.selectbox('What Is Your Education Level?',['Less Than Highschool','High School - No Diploma','High School Graduate','Some College','Two-Year Degree',
+    'Four-Year Degree','Some Postgraduate','Postgraduate Degree'])
+    parent = st.selectbox('Are You a Parent?', ['Yes','No'])
+if income == '<$10k':
+    income = 1
+elif income == '$10k-$20k':
+    income = 2
+elif income == '$20k-$30k':
+    income = 3
+elif income == '$30k-$40k':
+    income = 4
+elif income == '$40k-$50k':
+    income = 5
+elif income == '$50k-$75k':
+    income = 6
+elif income == '$75k-$100k':
+    income = 7
+elif income == '$100k-$150k':
+    income = 8
+else:
+    income = 9
+
+if education == 'Less Than Highschool':
+    education = 1
+elif education =='High School - No Diploma':
+    education = 2
+elif education == 'High School Graduate':
+    education = 3
+elif education == 'Some College':
+    education = 4
+elif education == 'Two-Year Degree':
+    education = 5
+elif education == 'Four-Year Degree':
+    education = 6
+elif education == 'Some Postgraduate':
+    education = 7
+else:
+    education = 8
+
+if parent == 'Yes':
+    parent = 1
+else:
+    parent = 0
 
 with col2:
-    married = st.slider('Married (1 = yes,0 = No)',0,1,1)
-    female = st.slider('Gender(0 = Male, 1 = Female)',0,1,1)
-    age = st.slider('Age',1,98,1)
+    married = st.selectbox('Are You Married?',['Married','Not Married'])
+    female = st.selectbox('Do You Identify as Male or Female?',['Male','Female'])
+    age = st.number_input('Input Your Age', min_value=1, max_value=98,step=1)
+
+if married == 'Married':
+    married = 1
+else:
+    married = 0
+
+if female == 'Female':
+    female = 1
+else:
+    female = 0
 
 
-
-if st.button("Predict if LinkedIn User"):
+    
+if st.button("Predict If LinkedIn User"):
     result = predict(np.array([[income, education, parent, married, female, age]]))
     prob = probs(np.array([[income, education, parent, married, female, age]]))
-    st.text(f"Prediction: **{result[0]}** (1 is 'User', 0 is 'NOT a User')")
-    st.text(f"Probability that this person is a LinkedIn user: {prob[0][1]}")
+    if result==1:
+        print(st.text("Prediction: IS a LinkedIn User"))
+        st.text(f"Probability that this person IS a LinkedIn user: {prob[0][1]}")
+    else:
+        print(st.text("Prediction: Is NOT a LinkedIn User"))
+        st.text(f"Probability that this person is NOT a LinkedIn user: {prob[0][1]}")
+    
+
+
+
+
+    
+    
+
+
+ 
+
+
+
+
+
+
+
+
+
+
 
 
 
